1be95f2e8b98af3cfe9071014b14a3b26f2b4a03
Renan Lazarotto <renanlazarotto@gmail.com>
2024-02-14 16:13:06 -0300

    Correção na configuração do Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,svelte,ts,md}'],
-  extend: {
-    fontFamily: {
-      'sans': ['"Mona Sans"', ...defaultTheme.fontFamily.sans],
+  content: ["./src/**/*.{html,svelte,ts,md}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Mona Sans"', ...defaultTheme.fontFamily.sans],
+      },
     },
   },
+
   plugins: [],
-}
+};
