d8ce4cb739f565eb8f1a9d984ce5f476b2e2f7a3
Renan Lazarotto <renanlazarotto@gmail.com>
2024-05-16 10:01:18 -0300

    Move o Header e o footer pra componentes

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+    import Link from "$lib/components/Link.svelte";
+
+    import { version } from "$app/environment";
+</script>
+
+<footer>
+    <dl class="grid grid-cols-[max-content_minmax(0,1fr)] text-sm">
+        <dt
+            class="py-1 pr-4 text-neutral-500 [&:not(:first-of-type)]:border-t [&:not(:first-of-type)]:border-neutral-600 [&:not(:first-of-type)]:border-dashed"
+        >
+            Desenvolvido por
+        </dt>
+        <dd
+            class="py-1 pr-4 [&:not(:first-of-type)]:border-t [&:not(:first-of-type)]:border-neutral-600 [&:not(:first-of-type)]:border-dashed"
+        >
+            Renan Lazarotto
+        </dd>
+        <dt
+            class="py-1 pr-4 text-neutral-500 [&:not(:first-of-type)]:border-t [&:not(:first-of-type)]:border-neutral-600 [&:not(:first-of-type)]:border-dashed"
+        >
+            Código fonte
+        </dt>
+        <dd
+            class="py-1 pr-4 [&:not(:first-of-type)]:border-t [&:not(:first-of-type)]:border-neutral-600 [&:not(:first-of-type)]:border-dashed"
+        >
+            <Link href="https://github.com/RenanLazarotto/rlazarotto" target="_blank">RenanLazarotto/rlazarotto</Link>
+        </dd>
+
+        <dt
+            class="py-1 pr-4 text-neutral-500 [&:not(:first-of-type)]:border-t [&:not(:first-of-type)]:border-neutral-600 [&:not(:first-of-type)]:border-dashed"
+        >
+            Versão
+        </dt>
+        <dd
+            class="py-1 pr-4 [&:not(:first-of-type)]:border-t [&:not(:first-of-type)]:border-neutral-600 [&:not(:first-of-type)]:border-dashed"
+        >
+            {version}
+        </dd>
+    </dl>
+</footer>
---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import Link from "./Link.svelte";
+
+    export let pages: Types.Page[];
+</script>
+
+<header class="mb-8 flex justify-between items-center">
+    <a href="/" class="text-purple-600 hover:text-purple-400">
+        <h1 class="font-compact text-5xl tracking-wide">Renan Lazarotto</h1>
+        <div class="uppercase text-lg font-bold text-gray-500 tracking-widest">full stack developer</div></a
+    >
+
+    <nav>
+        <ul class="flex gap-2">
+            {#each pages as page}
+                <li>
+                    <Link href={`/pages/${page.slug}`} class="font-compact text-3xl">
+                        {page.title}
+                    </Link>
+                </li>
+            {/each}
+        </ul>
+    </nav>
+</header>
