bd96edb46f68821a401a8224fb0cdda3eb59c32f
Renan Lazarotto <renanlazarotto@gmail.com>
2024-05-17 17:14:16 -0300

    Normalização das cores usadas

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -6,12 +7,30 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ["'Archivo Variable'", ...defaultTheme.fontFamily.sans],
+                sans: ["'Onest Variable'", ...defaultTheme.fontFamily.sans],
             },
         },
-        fontFamily: {
-            display: ["'Big Shoulders Stencil Display Variable'"],
-            compact: ["'Bebas Neue'", ...defaultTheme.fontFamily.sans],
+        colors: {
+            transparent: "transparent",
+            current: "currentColor",
+            black: colors.black,
+            white: colors.white,
+            gray: colors.gray,
+            purple: colors.purple,
+            violet: colors.violet,
+            mint: {
+                50: "#edfff8",
+                100: "#d5fff0",
+                200: "#aeffe1",
+                300: "#70ffcc",
+                400: "#2bfdb0",
+                500: "#00f59b",
+                600: "#00c075",
+                700: "#00965f",
+                800: "#06754d",
+                900: "#076042",
+                950: "#003724",
+            },
         },
     },
 };
