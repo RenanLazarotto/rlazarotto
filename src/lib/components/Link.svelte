8a005ea83d34274693f26a6a493c1d861f3fd871
Renan Lazarotto <renanlazarotto@gmail.com>
2024-05-17 17:16:14 -0300

    Melhorias no component de Link

--- src/lib/components/Link.svelte
+++ src/lib/components/Link.svelte
@@ -6,11 +6,7 @@
 <a
     {href}
     {target}
-    class={`text-purple-400 underline underline-offset-4 decoration-dotted decoration-purple-400 hover:text-purple-600 hover:decoration-purple-600 ${$$props.class}`}
+    class={`font-bold underline underline-offset-2 decoration-1 text-mint-500 hover:text-mint-300 ${$$props.class}`}
 >
-    {#if target == "_blank"}
-        <slot /> <span class="material-symbols-rounded text-xs"> open_in_new </span>
-    {:else}
-        <slot />
-    {/if}
+    <slot />
 </a>
