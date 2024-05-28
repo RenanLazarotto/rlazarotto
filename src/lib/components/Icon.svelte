430b455d10f1b1150ceab10870be5e68ff247738
Renan Lazarotto <renanlazarotto@gmail.com>
2024-05-28 01:03:43 -0300

    Adiciona compontente de ícones

---
+++ src/lib/components/Icon.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    export let id: string;
+    export let width: number = 36;
+    export let height: number = 36;
+</script>
+
+<svg class="pointer-events-none {$$props.class}" {width} {height}>
+    <use href="#icon-{id}" />
+</svg>
