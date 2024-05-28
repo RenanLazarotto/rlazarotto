d017c24436ad3eeed89f5443ff4b2adadbdaf44b
Renan Lazarotto <renanlazarotto@gmail.com>
2024-05-28 01:04:25 -0300

    Remove o efeito de brilho do Header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -5,19 +5,7 @@
 </script>
 
 <header class="mb-16 flex justify-between items-center">
-    <div class="flex-1 block relative group h-10">
-        <p
-            class="uppercase absolute font-bold text-4xl tracking-wide text-mint-400 group-hover:text-purple-500 blur-[8px] z-20"
-        >
-            Renan Lazarotto
-        </p>
-        <a
-            href="/"
-            class="absolute z-30 text-mint-100 group-hover:text-purple-400 uppercase font-bold text-4xl tracking-wide"
-        >
-            Renan Lazarotto
-        </a>
-    </div>
+    <Link href="/" class="uppercase font-bold text-4xl tracking-wide">Renan Lazarotto</Link>
 
     <nav class="flex justify-center">
         <ul class="inline-flex gap-2">
