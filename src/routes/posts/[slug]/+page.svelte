c6a8024e37b4495c284d23333c5a1c999f24a7bb
Renan Lazarotto <renanlazarotto@gmail.com>
2024-05-28 01:17:01 -0300

    Altera o layout da página de visualização de posts

--- src/routes/posts/[slug]/+page.svelte
+++ src/routes/posts/[slug]/+page.svelte
@@ -11,22 +11,24 @@
     <meta property="og:title" content={data.meta.title} />
 </svelte:head>
 
-<h2 class="font-bold text-4xl mt-5 text-purple-100">{data.meta.title}</h2>
-<p class="mt-2 mb-4 text-purple-500 text-lg">
-    {data.meta.description}
-</p>
-<div class="flex gap-6 mb-12 text-xs">
-    <p class="text-purple-400">
-        <FormattedDate date={data.meta.published} class="font-medium" />
-        • <b>{data.meta.readingTime} min.</b> de leitura
+<article class="max-w-screen-lg mx-auto">
+    <h2 class="font-bold text-4xl mt-5 text-purple-100">{data.meta.title}</h2>
+    <p class="mt-2 mb-4 text-mint-500 text-lg">
+        {data.meta.description}
     </p>
-    {#if data.meta.updated}
-        <p class="text-neutral-500">
-            Última atualização em <FormattedDate date={data.meta.updated} class="font-medium" />
+    <div class="flex gap-6 mb-12 text-xs">
+        <p class="text-mint-300">
+            <FormattedDate date={data.meta.published} class="font-medium" />
+            • <b>{data.meta.readingTime} min.</b> de leitura
         </p>
-    {/if}
-</div>
+        {#if data.meta.updated}
+            <p class="text-neutral-500">
+                Última atualização em <FormattedDate date={data.meta.updated} class="font-medium" />
+            </p>
+        {/if}
+    </div>
 
-<article class="article mb-12">
-    <svelte:component this={data.content} />
+    <section class="article mb-12">
+        <svelte:component this={data.content} />
+    </section>
 </article>
