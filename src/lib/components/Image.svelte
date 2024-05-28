76c9fa3e8fa3aa70a706eba52722b09069ac4cbb
Renan Lazarotto <renanlazarotto@gmail.com>
2024-05-28 10:31:44 -0300

    Remove o componente Image

--- src/lib/components/Image.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    export let src: string;
-    export let alt: string = "";
-</script>
-
-<div class="mb-4">
-    <img {src} {alt} class={`border-4 border-white rounded ${$$props.class}`} />
-    <slot />
-</div>
