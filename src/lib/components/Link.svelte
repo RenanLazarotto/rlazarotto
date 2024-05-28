78c6a58076becc87443a5f9e22c3e6b241ad6a09
Renan Lazarotto <renanlazarotto@gmail.com>
2024-05-28 01:04:44 -0300

    Adiciona a opção de mostrar fundo no componente Link

--- src/lib/components/Link.svelte
+++ src/lib/components/Link.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
     export let href: string;
     export let target: string = "_self";
+    export let showBackground: boolean = false;
+
+    let background = showBackground ? "py-0.5 px-1 rounded bg-mint-700/20 hover:bg-purple-700/20" : "";
 </script>
 
 <a
     {href}
     {target}
-    class={`font-bold underline underline-offset-2 decoration-1 text-mint-500 hover:text-mint-300 ${$$props.class}`}
+    class:text-mint-300={!showBackground}
+    class:text-mint-500={showBackground}
+    class={`font-bold hover:text-purple-400 ${background} ${$$props.class}`}
 >
     <slot />
 </a>
