8876b4f76e59deae251bceed2bf2eb7ebf3f620f
Renan "Firehawk" Lazarotto <renanlazarotto@gmail.com>
2024-08-13 18:28:10 -0300

    Pequena alteração no cabeçalho do post para melhorar a visibilidade

--- src/routes/posts/[slug]/+page.svelte
+++ src/routes/posts/[slug]/+page.svelte
@@ -13,8 +13,8 @@
 </svelte:head>
 
 <article class="mx-auto">
-    <h2 class="font-medium text-3xl my-5 text-gray-100">{data.meta.title}</h2>
-    <p class="mb-3 text-gray-500">
+    <h2 class="font-bold text-3xl my-5 text-gray-100">{data.meta.title}</h2>
+    <p class="mb-3 text-gray-400 font-medium">
         {data.meta.description}
     </p>
     <div class="flex gap-2 mb-12 text-xs text-gray-500">
